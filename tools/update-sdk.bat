a1c961930dec5db0a8df86541973a89ea4440f06
xshady <54737754+xxshady@users.noreply.github.com>
2022-11-24 12:53:04 +0000

    Add current commit output to update-ask scripts

--- tools/update-sdk.bat
+++ tools/update-sdk.bat
@@ -1,3 +1,4 @@
 @echo off
 
 git submodule update --init --remote --merge --recursive deps/cpp-sdk
+cd deps/cpp-sdk && echo "current sdk commit:" && git log -1
--- tools/update-sdk.sh
+++ tools/update-sdk.sh
@@ -1,1 +1,2 @@
 git submodule update --init --remote --merge --recursive deps/cpp-sdk
+cd deps/cpp-sdk && echo "current sdk commit:" && git log -1
